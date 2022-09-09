8728dc0283ce4bbd7148440a239e1963c5b6f397
mohamed <karls.eins@gmail.comgit config --global user.name mohamedgit config --global user.email karls.eins@gmail.com>
2022-09-09 09:41:10 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-/*
- * main - print the string in the puts function
+/**
+ * main-Entry point
  *
- * Descrition: using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
- */
-int main (void)
+ * Return:Always 0 (Success)
+*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
