990ce4c6cc39d8292942b9a692676475a3e840cb
mohamed <karls.eins@gmail.comgit config --global user.name mohamedgit config --global user.email karls.eins@gmail.com>
2022-09-11 08:14:16 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/v2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * nain - Entry point
+ * Return: Always 0 (Success!)
+ */
+
+int main(void)
+{
+	char low;
+
+		for (low = 'a'; low <= 'z'; low++)
+			putchar(low);
+		putchar('\n');
+		return (0);
+}
+
