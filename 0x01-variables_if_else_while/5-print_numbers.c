2d54a402b9a5ebceef11a139cf4cac5cb76e002e
mohamed <karls.eins@gmail.comgit config --global user.name mohamedgit config --global user.email karls.eins@gmail.com>
2022-09-12 08:12:41 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,10 +8,12 @@
 
 int main(void)
 {
-	int i
-		for (i = 0; i <= 9; i++)
-		{
-			printf("%d\n", i);
-		}
-	Return(0);
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
 }
