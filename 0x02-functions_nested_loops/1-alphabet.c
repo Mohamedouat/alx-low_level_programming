3e4c887b64fe9b153009b589ff3b46857c591e27
mohamed <karls.eins@gmail.comgit config --global user.name mohamedgit config --global user.email karls.eins@gmail.com>
2022-09-14 13:53:17 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- *  main - print alphabet
- *  Return: Always 0
+ * print_alphabet - print alphabet
+ * Return: Always 0
  */
-
 void print_alphabet(void)
 {
 	char c;
 
-        for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-
-	}
-	_putchar('\n');
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
